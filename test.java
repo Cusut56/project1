2d730c2c8544ee7cd35cd2213fc4f5b0adc8b462
Cusut56 <tarashere2005@yahoo.com>
2015-10-21 20:13:35 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,27 @@
 ///// test
+float goldx,goldy;
+float lestatx=10;
+float lestaty=10;    //position of creature
+float hermanx=50;
+float hermany=100;    //position of monster
+float dx=250;
+float dy=150;
+float  horizon=  height/4;
+
+int count=0;
+//size:
+
+
+void setup(){
+  size(1000,820);  
+   background(75,175,100);
+  horizon=  height/4;
+ lestatx=width/2;
+  hermany=height/2;
+
+}
+void draw(){   //lestat
+fill(0);
+rect(dx,dy,50,140);
+ellipse(
+}
